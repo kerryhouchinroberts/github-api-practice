958bd351671a4363eaba585a2ada54142702f735
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-03-11 14:23:16 -0600

    Move everything to the index page and remove search page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,6 @@
 class WelcomeController < ApplicationController
 
   def index
-    @gh = GithubFetcher.new
-    @user = @gh.client.user
-  end
-
-  def search
     gh = GithubFetcher.new
     @user = gh.client.user(params[:user])
     @repo = GithubFetcher.new().user_repos(params[:user])
